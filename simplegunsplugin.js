8fe521fc4ab61269ce65bff85dd732b1b5c583c0
JustWhatever10101 <110840385+JustWhatever10101@users.noreply.github.com>
2024-02-17 14:24:17 -0500

    Add files via upload

---
+++ simplegunsplugin.js
@@ -0,0 +1,43 @@
+/*
+    This code is to see what function do. This will not be added to your project when compiled
+ */
+export class Player {
+    constructor(data) {
+        this.uid = data.uid
+    }
+
+
+}
+export class KeybindEvent {
+    constructor(keypressevent) {
+        this.control = keypressevent.ctrlKey
+        this.alt = keypressevent.altKey
+        this.keycode = keypressevent.code
+        this.key = keypressevent.key
+        this.shift = keypressevent.shiftKey
+    }
+}
+export function createKeybind(key,callback) {
+    window.addEventListener("keypress",(e) => {
+        if(e.key == key) {
+            callback(new KeybindEvent(e))
+        }
+    })
+}
+export class Plugin {
+    constructor(plugininfo) {
+        this.info = plugininfo
+    }
+    load() {
+        console.log("Loaded " + this.info.name)
+    }
+    tick(data) {
+    //     No default code
+    }
+    joingame() {
+        //     No default code
+    }
+    rendertick() {
+        //     No default code
+    }
+}
